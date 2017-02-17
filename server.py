5f59d6410d72751a364fd2b8494105464d8b6404
Uggla <uggla@free.fr>
2017-02-17 17:20:02 +0100

    Improvements

--- server.py
+++ server.py
@@ -1,12 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
 from flask import Flask
 from flask import jsonify
 from flask import request
 from flask_sockets import Sockets
+import jinja2
 import time
+from api_exception import InvalidUsage
 
 
 app = Flask(__name__)
 sockets = Sockets(app)
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 
 @sockets.route('/echo')
@@ -31,20 +43,47 @@ def bla_socket(ws):
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    html = render_template("main.template")
+    return html
+
+
+@app.route('/boot')
+def boot():
+    script = render_template("boot.template")
+    return script
 
 
 @app.route('/deploy/complete', methods=["POST"])
 def deploy_complete():
     # Write a trace file
-    macaddress = request.json['macaddress']
-    with open(macaddress, 'w') as tracefile:
+    try:
+        macaddress = request.json['macaddress']
+    except KeyError:
+        raise InvalidUsage(
+                'Invalid key provided should be macaddress', status_code=400)
+
+    filename = "flags/" + macaddress
+    with open(filename, 'w') as tracefile:
         tracefile.close()
         data = {"status": "ok"}
         resp = jsonify(data)
         resp.status_code = 200
         return resp
 
+
+def render_template(template):
+    # Initialize Template system (jinja2)
+    templates_path = 'templates'
+    jinja2_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(templates_path))
+    try:
+        template = jinja2_env.get_template(template)
+    except jinja2.exceptions.TemplateNotFound as e:
+        print('Template "{}" not found in {}.'
+              .format(e.message, jinja2_env.loader.searchpath[0]))
+
+    data = template.render()
+    return data
 
 if __name__ == "__main__":
     from gevent import pywsgi
